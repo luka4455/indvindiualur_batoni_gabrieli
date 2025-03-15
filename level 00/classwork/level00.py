60f035c9419479946d0ddc86dca9d4aca2404ef0
luka4455 <lukaula0402@gmail.com>
2025-03-15 19:31:33 +0400

    commit

---
+++ level 00/classwork/level00.py
@@ -0,0 +1,10 @@
+
+
+#2) შექმენი 5 ცვლადი input() ფუნქციის გამოყენებით და შემდეგ შენს მიერ შეყვანილი ინფორმაცია გამოიტანე print() ფუნქციის გამოყენებით
+name = input("enter your name:")
+username = input("enter your username:")
+age = input("enter your age:")
+love_number = input("enter your love number:")
+mom_name = input("enter your mom's name:")
+
+print("my name is:" + ' ' + name + ' ' + "my username is:" + ' ' + username + ' ' + "my age is:" + ' ' + age + ' ' + "my love number is" + ' ' + love_number + ' ' + "my mom's name is" + ' ' + mom_name)
---
+++ level 00/classwork/levell00.py
@@ -0,0 +1,10 @@
+#level 1 classwork: 1) შექმენი 5 რიცხვის ცვლადი და თითოეულზე მოქხდინ მათემატიკური ოპერაციები,  5 სტრინგის  ცვლადი და თითოეულზე  მოახდიენე კონკატენაცია(სტრინგების შეერთება) snake_case-ის გამოყენებით 
+number = 5
+number2 = 9
+number3 = 0
+number4 = 10
+number5 = 11
+
+number_number = number + number2 + number3 + number4 + number5
+
+print(number_number)
