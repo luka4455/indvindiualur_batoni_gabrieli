73b4a4efbe0f235895211616afa3c3b0c33628de
luka4455 <lukaula0402@gmail.com>
2025-05-14 10:46:18 +0400

    Add files via upload

---
+++ indvidual gabrieli/level 2/homework/homework.py
@@ -0,0 +1,17 @@
+#1
+num = 11
+num2 = 22
+
+print(type(num))
+print(type(num2))
+
+#2
+name = "luka"
+name2 = "gabrieli"
+
+print(("my name is " + name + " and my thicher name is " + name2))
+
+#3
+#type() ამოწმებს თუ ცვლადში რა არის მაგალითად <"class int">, <"class str">, <"class float">, <"class bool"> და ჩვენ ვიყენებთ რომ გავიგოთ თუ ცვლადში რა არის.
+
+
---
+++ indvidual gabrieli/level 3/homevork/homework.py
@@ -0,0 +1,11 @@
+name = "11"
+name2 = "15" 
+name3 = "41"
+name4 = "61"
+name5 = "64"
+
+print(int(name))
+print(int(name2))
+print(int(name3))
+print(int(name4))
+print(int(name5))
